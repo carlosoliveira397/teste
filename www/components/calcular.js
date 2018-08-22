eb384083a543c7d10b304c12d5e6379bffded771
carlosoliveira397 <carlosd.oliveira397@gmail.com>
2018-08-23 00:53:32 +0900

    Project's Initial Commit

---
+++ www/components/calcular.js
@@ -0,0 +1,41 @@
+// This is a JavaScript file
+    
+    $(document).on("click", "#um", function(){
+    $("#result").val(1);
+    });
+    
+    $(document).on("click", "#dois", function(){
+    $("#result").val(2);
+    });
+    
+    $(document).on("click", "#tres", function(){
+    $("#result").val(3);
+    });
+    
+    $(document).on("click", "#quatro", function(){
+    $("#result").val(4);
+    });
+    
+    $(document).on("click", "#cinco", function(){
+    $("#result").val(5);
+    });
+    
+    $(document).on("click", "#seis", function(){
+    $("#result").val(6);
+    });
+    
+    $(document).on("click", "#sete", function(){
+    $("#result").val(7);
+    });
+    
+    $(document).on("click", "#oito", function(){
+    $("#result").val(8);
+    });
+    
+    $(document).on("click", "#nove", function(){
+    $("#result").val(9);
+    });
+    
+    $(document).on("click", "#zero", function(){
+    $("#result").val(0);
+    });
